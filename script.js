293e1786d3b2292f2ecb869d02b411e0df113baf
ernyeager64-del <ernyeager64@gmail.com>
2025-12-14 20:47:43 +0800

    Update script.js

--- script.js
+++ script.js
@@ -2,19 +2,22 @@
 const loginForm = document.getElementById('loginForm');
 const loginMessage = document.getElementById('loginMessage');
 
-loginForm.addEventListener('submit', (e) => {
-    e.preventDefault();
+if (loginForm) {
+    loginForm.addEventListener('submit', (e) => {
+        e.preventDefault();
 
-    const username = loginForm[0].value.trim();
-    const password = loginForm[1].value.trim();
+        const username = loginForm[0].value.trim();
+        const password = loginForm[1].value.trim();
 
-    if(username === "student" && password === "1234"){
-        loginMessage.textContent = "Login successful! Welcome, " + username;
-        loginMessage.style.color = "green";
-    } else {
-        loginMessage.textContent = "Invalid username or password!";
-        loginMessage.style.color = "red";
-    }
+        if(username === "student" && password === "1234"){
+            loginMessage.textContent = "Login successful! Welcome, " + username;
+            loginMessage.style.color = "green";
+        } else {
+            loginMessage.textContent = "Invalid username or password!";
+            loginMessage.style.color = "red";
+        }
 
-    loginForm.reset();
-});
+        loginForm.reset();
+    });
+}
+
