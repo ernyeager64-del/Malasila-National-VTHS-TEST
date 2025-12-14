8edddaf099e10d18f6b51df290d5ea2497ecdb90
ernyeager64-del <ernyeager64@gmail.com>
2025-12-14 21:00:17 +0800

    Add files via upload

--- script.js
+++ script.js
@@ -1,14 +1,11 @@
-// Login form simulation
 const loginForm = document.getElementById('loginForm');
 const loginMessage = document.getElementById('loginMessage');
 
 if (loginForm) {
     loginForm.addEventListener('submit', (e) => {
         e.preventDefault();
-
         const username = loginForm[0].value.trim();
         const password = loginForm[1].value.trim();
-
         if(username === "student" && password === "1234"){
             loginMessage.textContent = "Login successful! Welcome, " + username;
             loginMessage.style.color = "green";
@@ -16,8 +13,6 @@ if (loginForm) {
             loginMessage.textContent = "Invalid username or password!";
             loginMessage.style.color = "red";
         }
-
         loginForm.reset();
     });
 }
-
